7a03eae2705076b5b207656b43a906694ddd3fab
humorous-storage-7521 <rmwerner@umich.edu>
2022-10-02 22:08:23 -0500

    add title and links

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-const projects = document.querySelectorAll('.project')
+const projectDivs = document.querySelectorAll('.project')
+const frontendLink = document.getElementById('frontend-link')
+const backendLink = document.getElementById('backend-link')
+const projectName = document.getElementById('name')
 
-projects.forEach(project => {
+const frontends = [
+    'https://github.com/rmwerner18/chord_mixer_client',
+    'https://github.com/rmwerner18/some_trails_client',
+    'https://github.com/rmwerner18/twitter_project_frontend'
+]
+
+const projects = {
+    1: {
+        name: 'CHORD MIXER',
+        frontend: 'https://github.com/rmwerner18/chord_mixer_client',
+        backend: 'https://github.com/rmwerner18/chord_mixer_api'
+    },
+    2: {
+        name: 'SOME TRAILS',
+        frontend: 'https://github.com/rmwerner18/some_trails_client',
+        backend: 'https://github.com/rmwerner18/some_trails_api'
+    },
+    3: {
+        name: 'TWITTER CLOUD',
+        frontend: 'https://github.com/rmwerner18/twitter_project_frontend',
+        backend: 'https://github.com/rmwerner18/twitter_project_backend'
+    }
+}
+
+const backends = [
+    'https://github.com/rmwerner18/chord_mixer_api',
+    'https://github.com/rmwerner18/some_trails_api',
+    'https://github.com/rmwerner18/twitter_project_backend'
+]
+
+projectDivs.forEach(project => {
     project.addEventListener('click',() => {
         removeActiveClasses()
         project.classList.add('active')
+
+        frontendLink.setAttribute('href', projects[project.id].frontend)
+        backendLink.setAttribute('href', projects[project.id].backend)
+        projectName.innerText = projects[project.id].name
     })
 })
 
 const removeActiveClasses = () => {
-    projects.forEach(project => {
+    projectDivs.forEach(project => {
         project.classList.remove('active')
     })
 }
